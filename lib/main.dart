d22bd967afc56d5aaaf9b924bff6f7bfd6052d1d
Tony Wang <yilongwang05@gmail.com>
2026-03-13 11:16:30 -0700

    feat: implement truly silent background Google Sign-In on home page

--- lib/main.dart
+++ lib/main.dart
@@ -162,6 +162,9 @@ class _DashboardScreenState extends State<DashboardScreen> {
         });
       }
     });
+
+    // Trigger silent sign-in in the background
+    GoogleSignIn.instance.attemptLightweightAuthentication();
   }
 
   @override
